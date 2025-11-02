6b6d57e12dd9214807ca70848a4d6d80f3a3590d
annseldhose <annseldhose1328@gmail.com>
2025-11-02 22:01:50 +0530

    Fix typo in PortfolioVideos component name

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Services />
       <Industries />
       <WhyChooseUs />
-      <Portfoliovideos />
+      <PortfolioVideos />
       <Portfolio />
       <Footer />
     </div>
